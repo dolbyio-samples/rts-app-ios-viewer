79a96922e3b23889c0476cbed2ab86babc4ca0aa
Aravind Raveendran <aravind.raveendran@dolby.com>
2022-12-21 16:32:11 +1100

    Rename project, target and schemes to `RTSViewer*`

--- RTSViewer/RTSViewer.swift
+++ RTSViewer/RTSViewer.swift
@@ -1,15 +1,10 @@
-//
-//  Millicast_SDK_Sample_App_in_SwiftApp.swift
-//  Millicast SDK Sample App in Swift
-//
-
 import SwiftUI
 
 /**
  * Entry point to the Sample Application (SA) for the Millicast iOS SDK.
  */
 @main
-struct Millicast_SDK_Sample_App_in_SwiftApp: App {
+struct RTSViewer: App {
     @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
