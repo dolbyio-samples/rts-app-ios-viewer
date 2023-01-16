bada1e164d19dcc0204fd063ca6de29071b31545
Ramesh Prasad <ramesh.prasad@dolby.com>
2023-01-16 13:40:08 +1100

    settings view

--- RTSViewer/StreamingScreen/StreamingScreen.swift
+++ RTSViewer/StreamingScreen/StreamingScreen.swift
@@ -7,12 +7,21 @@ import DolbyIOUIKit
 import SwiftUI
 import RTSComponentKit
 
+enum StreamType: String, CaseIterable, Identifiable {
+    case auto, high, medium, low
+    var id: Self { self }
+}
+
 struct StreamingScreen: View {
 
     private let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
-
+    
     @EnvironmentObject private var dataStore: RTSDataStore
     @State private var volume = 0.5
+    @State private var mediaStats = false
+    @State private var liveIndicator = false
+    @State private var selectedFlavor: StreamType = .auto
+    @State private var showSettings = false
 
     var body: some View {
         BackgroundContainerView {
@@ -24,9 +33,34 @@ struct StreamingScreen: View {
                     .background(Color.black)
                     .opacity(isStreamActive ? 0.0 : 0.8)
 
+                if(showSettings) {
+                    VStack {
+                        VStack {
+                            Spacer().frame(width: Layout.spacing1x)
+                            List {
+                                
+                                if #available(tvOS 16.0, *) {
+                                    Picker("Simulcast", selection: $selectedFlavor) {
+                                        ForEach(StreamType.allCases, id: \.self) { item in // 4
+                                            Text(item.rawValue.capitalized) // 5
+                                        }
+                                    }.pickerStyle(.navigationLink)
+                                } else {
+                                    // Fallback on earlier versions
+                                }
+                                
+                                
+                                Toggle("Media stats", isOn: $mediaStats)
+                                Toggle("Live indicator", isOn:$liveIndicator)
+                            }
+                        }.frame(maxWidth: 800, maxHeight: 600, alignment: .bottom)
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                }
+                
                 VStack {
                     HStack {
-                        IconButton(name: .settings, tintColor: .white) {
+                        IconButton(name: .settings, tintColor: .white, focusedBackgroundColor: .white) {
+                            showSettings = !showSettings
                         }
                         Spacer().frame(width: Layout.spacing1x)
                     }.frame(maxWidth: .infinity, alignment: .trailing)
