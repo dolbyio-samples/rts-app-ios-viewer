721090e6ce6d18ca1f483c10e58d183291c35f46
Aravind Raveendran <aravind.raveendran@dolby.com>
2023-01-19 13:13:54 +1100

    configuration updates to limit code sign settings only to app targets

--- LocalPackages/DolbyIOUIKit/Package.swift
+++ LocalPackages/DolbyIOUIKit/Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DolbyIOUIKit",
+            type: .dynamic,
             targets: ["DolbyIOUIKit"])
     ],
     dependencies: [
