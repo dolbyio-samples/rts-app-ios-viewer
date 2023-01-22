3f2460d86eca48a40a233868bc9ff84ab230bf17
Liubov Danilina <liubov.danilina@dolby.com>
2023-01-23 08:52:51 +1100

    Statistics view UI changes

--- RTSViewer/StreamingScreen/StatisticsView.swift
+++ RTSViewer/StreamingScreen/StatisticsView.swift
@@ -15,25 +15,31 @@ struct StatisticsView: View {
     private let fontTable = Font.avenirNextRegular(withStyle: .caption2, size: FontSize.caption2)
 
     private let fontAssetCaption = FontAsset.avenirNextDemiBold(size: FontSize.caption1, style: .caption)
+    private let fontAssetTitle = FontAsset.avenirNextBold(size: FontSize.title3, style: .title3)
 
     var body: some View {
         VStack {
             VStack {
                 VStack {
                     HStack {
-                        Text(text: "stream.stats.name.label", fontAsset: fontAssetCaption).frame(maxWidth: 300, alignment: .leading)
+                        Text(text: "stream.media-stats.label", fontAsset: fontAssetTitle).frame(maxWidth: .infinity, alignment: .leading)
+                        Spacer().frame(width: Layout.spacing1x)
+                    }.padding([.leading, .top], 40).padding([.bottom], 25)
+                    HStack {
+                        Text(text: "stream.stats.name.label", fontAsset: fontAssetCaption).frame(maxWidth: 250, alignment: .leading)
                         Text(text: "stream.stats.value.label", fontAsset: fontAssetCaption)
-                    }.frame(maxWidth: .infinity, alignment: .leading).padding([.leading, .top], 40)
-                    List {
-                        ForEach(data) { item in
-                            HStack {
-                                Text(text: item.key, fontAsset: fontAssetTable).frame(maxWidth: 300, alignment: .leading)
-                                Text(item.value).font(fontTable)
-                            }
-                        }
+                    }.frame(maxWidth: .infinity, alignment: .leading).padding([.leading, .trailing], 40).padding([.bottom], 10)
+                    ForEach(data) { item in
+                        HStack {
+                            Text(text: item.key, fontAsset: fontAssetTable).frame(maxWidth: 250, alignment: .leading)
+                            Text(item.value).font(fontTable)
+                        }.frame(maxWidth: .infinity, alignment: .leading).padding([.leading, .trailing], 40)
                     }
+                    HStack {
+                        Spacer().frame(width: Layout.spacing1x)
+                    }.padding([.leading, .top], 40)
                 }.background(Color(uiColor: UIColor.Neutral.neutral800)).cornerRadius(Layout.cornerRadius6x)
-            }.frame(maxWidth: 700, maxHeight: 900).padding([.leading, .bottom], 35)
+            }.frame(maxWidth: 700, maxHeight: 700).padding([.leading, .bottom], 35)
         }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
     }
 
