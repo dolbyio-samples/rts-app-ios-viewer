080f287b7b4981e3df4c8910f066c295ef517779
Liubov Danilina <liubov.danilina@dolby.com>
2023-05-19 07:48:03 +1000

    reuse playbackView

--- LocalPackages/DolbyIORTSUIKit/Sources/DolbyIORTSCore/Model/StreamSourceViewProvider.swift
+++ LocalPackages/DolbyIORTSUIKit/Sources/DolbyIORTSCore/Model/StreamSourceViewProvider.swift
@@ -12,20 +12,40 @@ public protocol SourceViewProviding {
     var frameHeight: CGFloat { get }
 }
 
-struct StreamSourceViewProvider {
-    let renderer: MCIosVideoRenderer
+class StreamSourceViewProvider {
+//    let renderer: MCIosVideoRenderer
+    var renderer: MCIosVideoRenderer?
+    var view: UIView?
+    let track: MCVideoTrack
+//    init(renderer: MCIosVideoRenderer) {
+//        print("+++++++++++> renderer created: \(renderer)")
+//        self.renderer = renderer
+//    }
+    init(_ track: MCVideoTrack) {
+        self.track = track
+    }
 }
 
 extension StreamSourceViewProvider: SourceViewProviding {
     var frameWidth: CGFloat {
-        CGFloat(renderer.getWidth())
+        CGFloat(renderer?.getWidth() ?? 0)
     }
 
     var frameHeight: CGFloat {
-        CGFloat(renderer.getHeight())
+        CGFloat(renderer?.getHeight() ?? 0)
     }
 
     var playbackView: UIView {
-        renderer.getView()
+        if let view = self.view {
+            return view
+        } else {
+            let renderer = MCIosVideoRenderer()
+            track.add(renderer)
+            let view: UIView = renderer.getView()
+            self.view = view
+            self.renderer = renderer
+            print("+++++++++++> view created: \(view)")
+            return view
+        }
     }
 }
