2ec596bd1721b071dfb72e2cb59516deb2631ae0
Mok, Casper <casper.mok@dolby.com>
2023-06-19 11:00:33 +1000

    The app repo main branch uses main branch from UIKIt for constant development. A release branch with tag will be created for a stable app release.

--- LocalPackages/RTSComponentKit/Package.swift
+++ LocalPackages/RTSComponentKit/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/millicast/millicast-sdk-swift-package", from: "1.5.0"),
-        .package(url: "https://github.com/DolbyIO/rts-uikit-ios", from: "0.0.1")
+        .package(url: "https://github.com/DolbyIO/rts-uikit-ios", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
