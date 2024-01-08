d4b93febf26e592709eda45045f16f58c4649a38
Aravind Raveendran <aravind.raveendran@dolby.com>
2024-01-09 08:39:05 +1100

    Consume updated UIKit for 1.7.0 Fixes an issue showing accountID as screen title instead of stream name Add a toggle to control PiP

--- interactive-player/Interactive Viewer/iOS/Components/AppSettings/AppSettingsView.swift
+++ interactive-player/Interactive Viewer/iOS/Components/AppSettings/AppSettingsView.swift
@@ -11,12 +11,21 @@ import DolbyIOUIKit
 
 struct AppSettingsView: View {
     @AppConfiguration(\.showDebugFeatures) var showDebugFeatures
+    @AppConfiguration(\.enablePiP) var enablePiP
 
     var body: some View {
         // Custom App Configurations
         Toggle(isOn: $showDebugFeatures) {
             Text(
                 "app-configuration-show-debug-features-label",
+                style: .titleMedium,
+                font: .custom("AvenirNext-Regular", size: FontSize.body)
+            )
+        }
+
+        Toggle(isOn: $enablePiP) {
+            Text(
+                "app-configuration-enable-pip-features-label",
                 style: .titleMedium,
                 font: .custom("AvenirNext-Regular", size: FontSize.body)
             )
