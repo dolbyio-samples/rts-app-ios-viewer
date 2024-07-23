ce71097c8b678d33828e7877d20a9d2162691623
Sheila Doherty <sheila.doherty@dolby.com>
2024-07-23 15:36:14 -0600

    cleanup

--- interactive-player/Interactive Viewer/Views/SingleStream/SingleStreamViewModel.swift
+++ interactive-player/Interactive Viewer/Views/SingleStream/SingleStreamViewModel.swift
@@ -13,18 +13,18 @@ final class SingleStreamViewModel: ObservableObject {
         subsystem: Bundle.main.bundleIdentifier!,
         category: String(describing: SingleStreamViewModel.self)
     )
-
+    
     let sources: [StreamSource]
     let selectedVideoSource: StreamSource
     let selectedAudioSource: StreamSource?
     let settingsMode: SettingsMode
     let subscriptionManager: SubscriptionManager
     let videoTracksManager: VideoTracksManager
-
+    
     lazy var statsInfoViewModel = StatsInfoViewModel(streamSource: selectedVideoSource,
                                                      videoTracksManager: videoTracksManager,
                                                      subscriptionManager: subscriptionManager)
-
+    
     init(
         sources: [StreamSource],
         selectedVideoSource: StreamSource,
@@ -39,35 +39,9 @@ final class SingleStreamViewModel: ObservableObject {
         self.settingsMode = settingsMode
         self.subscriptionManager = subscriptionManager
         self.videoTracksManager = videoTracksManager
-
-//        observeStats()
     }
-
+    
     func streamSource(for id: UUID) -> StreamSource? {
         sources.first { $0.id == id }
     }
-
-//    private func observeStats() {
-//        Task { [weak self] in
-//            guard let self else { return }
-//            await self.subscriptionManager.$streamStatistics
-//                .receive(on: DispatchQueue.main)
-//                .sink { [weak self] statistics in
-//                    guard let self else { return }
-//                    self.streamStatistics = statistics
-//                }
-//                .store(in: &subscriptions)
-//
-//            await self.videoTracksManager.$targetBitrate
-//                .receive(on: DispatchQueue.main)
-//                .sink { [weak self] bitrate in
-//                    guard let self else { return }
-//                    if bitrate > 0 {
-//                        self.targetBitrate = bitrate
-//                    }
-//                }
-//                .store(in: &subscriptions)
-//        }
-//
-//    }
 }
