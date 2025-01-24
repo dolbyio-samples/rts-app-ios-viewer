0488fa3f74e0a807b8856a3e92734cdd667072d8
Yousif Khalid <yousif.khalid@dolby.com>
2025-01-24 15:08:03 +1100

    Use released version of sdk

--- LocalPackages/RTSCore/Package.swift
+++ LocalPackages/RTSCore/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/millicast/millicast-sdk-swift-package", .upToNextMajor(from: Version(2, 5, 2, prereleaseIdentifiers: ["beta"])))
+        .package(url: "https://github.com/millicast/millicast-sdk-swift-package", .upToNextMajor(from: Version(2, 5, 2)))
     ],
     targets: [
         .target(
